2d57bfc14d6a434bcd20ec919973e615cade19ce
B4rsam <105477761+B4rsam@users.noreply.github.com>
2023-12-04 15:20:33 +0330

    feat: Finished styling for now

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import './index.css'
 
 function App() {
   return (
-    <div className="flex flex-col items-center">
+    <div className="flex flex-col items-center pt-3">
       <ListButton>{(data, onDelete) => <div className="grid gap-1 grid-cols-4 bg-slate-500 p-5 rounded-lg m-2">{data.map(item => <Card key={item.id} cardData={item} onDelete={onDelete}/>)}</div>}</ListButton>
     </div>
   )
